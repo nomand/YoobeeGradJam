497979058d90e04a92b742caf54890e13c80e591
nomand <alexey.botkov@gmail.com>
2018-12-11 11:48:09 +1300

    drag stars wip

---
+++ Assets/Components/Scripts/DragTicker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragTicker : MonoBehaviour
+{
+    public int TicksPer360 = 180;
+
+    private int tickSpacing;
+    private int currentTick = 0;
+    private float tickCounter = 0;
+
+    Vector3 initialRotation;
+    Vector3 previousRotation;
+    Vector3 currentRotation;
+
+    void Start()
+    {
+        tickSpacing = 360 / TicksPer360;
+        initialRotation = transform.rotation.eulerAngles;
+        currentRotation = initialRotation;
+        previousRotation = initialRotation;
+    }
+
+    void Update()
+    {
+        currentRotation = transform.rotation.eulerAngles;
+
+        if (currentRotation.y != previousRotation.y)
+        {
+            tickCounter += previousRotation.y - currentRotation.y;
+            currentTick = (Mathf.RoundToInt(tickCounter * 100) / 100) / tickSpacing;
+        }
+
+        previousRotation = currentRotation;
+    }
+
+    void DoTick(int direction)
+    {
+        Debug.Log(currentTick);
+    }
+}
---
+++ Assets/Components/Scripts/RotateLayer.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class RotateLayer : MonoBehaviour
+{
+    private float rotationX;
+    private float rotationY;
+    private float rotationSpeed = 10;
+
+    Vector3 axis;
+
+    private void Start()
+    {
+        rotationX = transform.rotation.eulerAngles.y;
+        rotationY = transform.rotation.eulerAngles.x;
+        axis = transform.up;
+    }
+
+    private void Update()
+    {
+        if (Input.mousePosition.x == 0 ||
+            Input.mousePosition.y == 0 ||
+            Input.mousePosition.x >= Screen.width - 1 ||
+            Input.mousePosition.y >= Screen.height - 1)
+            return;
+
+        if(Input.GetMouseButtonDown(0))
+        {
+
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            rotationX += Input.GetAxis("Mouse X") * rotationSpeed;
+            rotationY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+            RotateView();
+        }
+
+        Debug.DrawLine(Vector3.zero, axis * 10);
+    }
+
+    public void RotateView()
+    {
+        transform.rotation = Quaternion.AngleAxis(rotationX, axis); //(Quaternion.AngleAxis(rotationX, Vector3.up) * Quaternion.AngleAxis(rotationY, Vector3.right));
+    }
+}
