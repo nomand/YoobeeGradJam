cd813992883573b47966dff9d1bacfaa55c7b61f
nomand <alexey.botkov@gmail.com>
2018-12-11 15:29:42 +1300

    angled orbits

---
+++ Assets/Components/Scripts/ConstellationManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstellationManager : MonoBehaviour
+{
+    public int stars;
+    public GameObject[] starObjects;
+
+    void Update()
+    {
+
+    }
+}
--- Assets/Components/Scripts/RotateLayer.cs
+++ Assets/Components/Scripts/RotateLayer.cs
@@ -2,6 +2,12 @@
 
 public class RotateLayer : MonoBehaviour
 {
+    public Transform[] layers;
+    int selection;
+    int lastSelection;
+
+    public PlanetSelector selector;
+
     private float rotationX;
     private float rotationY;
     private float rotationSpeed = 10;
@@ -12,11 +18,15 @@ public class RotateLayer : MonoBehaviour
     {
         rotationX = transform.rotation.eulerAngles.y;
         rotationY = transform.rotation.eulerAngles.x;
-        axis = transform.up;
+        axis = layers[selection].transform.up;
     }
 
     private void Update()
     {
+        selection = selector.currentSelection;
+        Debug.DrawRay(Vector3.zero, axis * 10);
+        axis = layers[selection].transform.up;
+
         if (Input.mousePosition.x == 0 ||
             Input.mousePosition.y == 0 ||
             Input.mousePosition.x >= Screen.width - 1 ||
@@ -25,21 +35,30 @@ public class RotateLayer : MonoBehaviour
 
         if(Input.GetMouseButtonDown(0))
         {
-
+            if(selection != lastSelection)
+            {
+                rotationX = 0;
+                rotationY = 0;
+            }
         }
 
         if (Input.GetMouseButton(0))
         {
-            rotationX += Input.GetAxis("Mouse X") * rotationSpeed;
-            rotationY -= Input.GetAxis("Mouse Y") * rotationSpeed;
+            rotationX = Input.GetAxis("Mouse X") * rotationSpeed;
+            rotationY = Input.GetAxis("Mouse Y") * rotationSpeed;
             RotateView();
         }
 
-        Debug.DrawLine(Vector3.zero, axis * 10);
+        if(Input.GetMouseButtonUp(0))
+        {
+            lastSelection = selection;
+        }
     }
 
     public void RotateView()
     {
-        transform.rotation = Quaternion.AngleAxis(rotationX, axis); //(Quaternion.AngleAxis(rotationX, Vector3.up) * Quaternion.AngleAxis(rotationY, Vector3.right));
+        var angle = layers[selection].rotation.eulerAngles.y + rotationX;
+
+        layers[selection].Rotate(axis, rotationX, Space.World); // = Quaternion.AngleAxis(angle, axis); //(Quaternion.AngleAxis(rotationX, Vector3.up) * Quaternion.AngleAxis(rotationY, Vector3.right));
     }
 }
