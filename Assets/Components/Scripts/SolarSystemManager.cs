af9a2e12ceafd2ef5cb1d0896b3b4d452e219500
nomand <alexey.botkov@gmail.com>
2018-12-12 20:10:48 +1300

    wip

---
+++ Assets/Components/Scripts/SolarSystemManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+public class SolarSystemManager : MonoBehaviour
+{
+    public GameObject[] Planets;
+    public GameObject UI;
+    public GameObject[] Orbits;
+
+    private Vector3[] UIPlanetPositions;
+
+    void Start()
+    {
+        PopulateUI();
+        PopulateOrbits();
+    }
+
+    private void PopulateOrbits()
+    {
+        for (int i = 0; i < Planets.Length; i++)
+        {
+
+        }
+    }
+
+    private void PopulateUI()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
