78daa9cca6f191f1e313728f75b2264e4fb21953
nomand <alexey.botkov@gmail.com>
2018-12-12 14:40:10 +1300

    constellation lines

--- Assets/Components/Scripts/ConstellationManager.cs
+++ Assets/Components/Scripts/ConstellationManager.cs
@@ -1,14 +1,35 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+[System.Serializable]
+public class StarGroups : IEnumerator, IEnumerable
+{
+    public GameObject[] Stars;
+    int position = -1;
+
+    public IEnumerator GetEnumerator(){ return (IEnumerator)this; }
+    public bool MoveNext() { position++; return (position < Stars.Length); }
+    public void Reset() { position = 0; }
+    public object Current { get { return Stars[position]; }}
+}
 
 public class ConstellationManager : MonoBehaviour
 {
-    public int stars;
-    public GameObject[] starObjects;
+    public GameObject linePrefab;
+    public StarGroups[] Group;
 
-    void Update()
+    private void Start()
     {
+        foreach(StarGroups group in Group)
+        {
+            var thisLine = Instantiate(linePrefab).GetComponent<UpdateConstellationLine>();
+            thisLine.gameObject.transform.parent = transform;
+            thisLine.stars = new GameObject[group.Stars.Length];
 
+            for (int i = 0; i < group.Stars.Length; i++)
+            {
+                thisLine.stars[i] = group.Stars[i];
+            }
+        }
     }
 }
---
+++ Assets/Components/Scripts/UpdateConstellationLine.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateConstellationLine : MonoBehaviour
+{
+    LineRenderer line;
+
+    public GameObject[] stars;
+
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        if (stars.Length == 0)
+            return;
+
+        for (int i = 0; i < stars.Length; i++)
+        {
+            line.SetPosition(i, stars[i].transform.position);
+        }
+    }
+}
